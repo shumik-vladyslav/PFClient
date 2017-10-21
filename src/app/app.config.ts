1c64597598cf13fcc662232ddeeeb46b17a803db
shumik-vladyslav <shumik.vladyslav@gmail.com>
2017-10-21 21:54:52 +0300

    fix client and chef edit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  api_prefix : 'http://localhost:5000/',
+  api_prefix : 'http://www.productive-families.com/',
   default_dish_img_url: 'https://s3.us-east-2.amazonaws.com/heroku-imgs/dish.png',
   default_human_img_url: 'https://s3.us-east-2.amazonaws.com/heroku-imgs/human.jpg'
 }
